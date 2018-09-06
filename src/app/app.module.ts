6e1fc0b73a6c771a147b3089d1a3ffa1372a627f
hlncg2@gmail.com <nhc17@gmail.com>
2018-09-06 17:19:31 +0800

    routing working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,10 +11,11 @@ import { MaterialModule } from './material.module';
 import { Weatherv2Service } from './weatherv2.service';
 import { HttpClientModule } from '@angular/common/http';
 import { environment } from '../environments/environment';
+import { CityComponent } from './component/city.component';
+import { DogComponent } from './component/dog.component';
+import { PageNotFoundComponent } from './component/page-not-found.component';
 
-
-
-
+import { RoutingModule } from './routing';
 
 
 
@@ -22,7 +23,9 @@ import { environment } from '../environments/environment';
 @NgModule({
   declarations: [
     AppComponent,
-    
+    CityComponent,
+    DogComponent, 
+    PageNotFoundComponent
   ],
 
   imports: [
@@ -31,7 +34,8 @@ import { environment } from '../environments/environment';
     FormsModule,
     ReactiveFormsModule,
     MaterialModule,
-    HttpClientModule
+    HttpClientModule,
+    RoutingModule
   ],
 
   providers: [ Weatherv2Service],
